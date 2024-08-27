036fe9768e28e8e4b758de4f14e3f27a0fdc2bfb
Venyus Boldrini <venyustech@gmail.com>
2024-08-27 15:44:05 -0300

    feat: create navbar

--- src/modules/shared/constants/colorTheme.ts
+++ src/modules/shared/constants/colorTheme.ts
@@ -1,32 +1,33 @@
 export const colors = {
   main: {
-    primary: '#ececec',
-    secundary: '#ffffff'
+    primary: '#FFFFFF',
+    secundary: '#ECECEC',
+    border: '#CCCCCC',
+    'border-selected': '#44A9CC'
   },
   form: {
     label: '#072854',
     'label-selected': '#072854',
     'primary-button': '#2797BA',
-    'primary-button-selected': '#2797BA',
     'secundary-button': '#2CBD62',
     'secundary-button-selected': '#258B4B'
   }
 }
 
-const createProxy = (obj: any, path: string[] = []): any =>
+const createProxy = <T extends object>(obj: T, path: string[] = []): T =>
   new Proxy(obj, {
-    get: (target, prop) => handleGet(target, prop, path)
+    get: (target, prop: string | symbol) => handleGet(target, prop, path)
   })
 
-const handleGet = (target: any, prop: string | symbol, path: string[]) => {
+const handleGet = <T extends object>(target: T, prop: string | symbol, path: string[]) => {
   if (typeof prop === 'string' && prop in target) {
-    const value = target[prop]
+    const value = target[prop as keyof T]
     return isObject(value) ? createProxy(value, [...path, prop]) : formatPath(path, prop)
   }
   return undefined
 }
 
-const isObject = (value: any) => typeof value === 'object' && value !== null
+const isObject = (value: any): value is object => typeof value === 'object' && value !== null
 
 const formatPath = (path: string[], prop: string): string => [...path, prop].join('.')
 
